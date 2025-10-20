77aafa6d66c5985b1927294e0d98152aea1d2f0d
Krish <krishc4005@gmail.com>
2025-10-20 22:56:42 +0530

    Made login page functional with axios

--- frontend/src/pages/user/UserLogin.jsx
+++ frontend/src/pages/user/UserLogin.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import '../../styles/styles.css';
 import FormField from '../../components/FormField';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const UserLogin = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+    const response = await axios.post("http://localhost:3000/api/auth/user/login", {
+      email : email,
+      password : password
+    }, {
+      withCredentials : true
+    })
+
+    console.log(response.data)
+
+    navigate("/");
+  }
+
   return (
     <div className="page login-page">
       <div className="brand">Spills</div>
       
       <div className="login-container">
-        <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="login-form" noValidate onSubmit={handleSubmit}>
           <h2 className="form-title">Welcome back</h2>
           <p className="form-sub">Sign in with your email and password.</p>
 
