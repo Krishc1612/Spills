eb689361962605ac1e10793d18ea2f527102f00c
Krish <krishc4005@gmail.com>
2025-10-20 22:48:02 +0530

    Added user login feature for backend routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -46,8 +46,57 @@ async function registerUser(req, res){
         console.error('registerUser error:', err)
         return res.status(500).json({ message: 'Internal server error', error: err.message })
     }
+
+    // did this inorder to detect any errors happening in the try block.
+    // elsewise we only had only used try and catch in jwt.verify which if correctly verifies the token allows the next statement to execute else it throws an error
+} 
+
+async function loginUser(req, res){
+    try {
+        const {email, password} = req.body
+
+        const user = await userModel.findOne({
+            email
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                message : "Username or password incorrect."
+            })
+        }
+
+        const validatePassword = await bcrypt.compare(user.password, password)
+
+        if (!validatePassword){
+            return res.status(400).json({
+                message : "Username or password incorrect."
+            })
+        }
+
+        const token = jwt.sign({
+            id : user._id
+        }, 
+            process.env.JWT_SECRET
+        )
+
+        res.cookie('token', token);
+
+        return res.status(200).json({
+            message: "User Logged In Successfully.",
+            user : {
+                email : email
+            }
+        })
+
+    } catch (err) {
+        console.log("Can't login user.")
+        return res.status(500).json({
+            message : "Error logging in user"
+        })
+    }
 } 
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
