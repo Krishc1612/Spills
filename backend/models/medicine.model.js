43ddd5c7a88ff77b33a448a9fcf4515fbfa686f9
Krish <krishc4005@gmail.com>
2025-10-21 19:07:58 +0530

    Added medicine model with basic schema for now

---
+++ backend/models/medicine.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const medicineSchema = mongoose.Schema({
+    medName : {
+        type : String,
+        required : true
+    },
+    dosage : {
+        type : String,
+        required : true
+    },
+    frequency : {
+        type : Number,
+        required : true
+    },
+    instructions : {
+        type : String
+    },
+    startDate : {
+        type : Date,
+        required : true
+    },
+    endDate : {
+        type : Date,
+        required : true
+    },
+    userId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "user",
+        required : true
+    }
+}, 
+{
+    timestamps : true
+});
+
+const medicineModel = mongoose.model("medicine", medicineSchema)
+
+module.exports = medicineModel
