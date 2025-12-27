dd40e292621cc006d458ad0b451261b9c64d6b73
Krish <krishc4005@gmail.com>
2025-12-27 16:56:50 +0530

    Fixed a subtle bug by adding bootstrap to prevent the database being connected later than expected during application startup.

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,17 @@ require('dotenv').config();
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
 
-connectDB();
+async function bootstrap() {
+    try {
+        await connectDB();
 
-app.listen(process.env.PORT, () => {
-    console.log("Server started on PORT.")
-}) // starting the app in server.js
+        app.listen(process.env.PORT, () => {
+            console.log("Server started on PORT.")
+        }) // starting the app in server.js
+    } catch (error) {
+        console.error("An error happened in bootstrap: ", error);
+        process.exit(1);
+    }
+}
+
+bootstrap();
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
-function connectDB(){
-    mongoose.connect(process.env.MONGO_URI)
+async function connectDB(){
+    await mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Connected to the Database.")
     }) .catch((err) => {
